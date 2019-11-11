cd9dc976902ad9fb43754bdfaa732e8eb500a23c
JCameron17 <jasmine.cameron@bison.howard.edu>
2019-11-10 23:40:11 -0500

    get user input

--- project2.s
+++ project2.s
@@ -1,4 +1,12 @@
-.data 
+.data
 #variable for user input
-myString: .space 32
+myString: .space 1000
 line: .asciiz "\n"
+
+.text
+main:
+
+    li $v0, 1000           #get user input
+    la $a0, myString    #store string
+    li $a1, 1001
+    syscall
